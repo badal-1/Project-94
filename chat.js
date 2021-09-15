bfc4d4f043201f92094abba3a6041c81479e64bf
badal-1 <81519542+badal-1@users.noreply.github.com>
2021-09-15 18:52:28 +0530

    Add files via upload

---
+++ chat.js
@@ -0,0 +1,14 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyATqAlRAmvynowBUagfi7ALE-WW75_VKbc",
+    authDomain: "project-e22a3.firebaseapp.com",
+    databaseURL: "https://project-e22a3-default-rtdb.firebaseio.com",
+    projectId: "project-e22a3",
+    storageBucket: "project-e22a3.appspot.com",
+    messagingSenderId: "746465396850",
+    appId: "1:746465396850:web:8469ec2748f34f94589aeb",
+    measurementId: "G-R9QPJMKQ9G"
+  };
+  
+  // Initialize Firebase
+  var app = initializeApp(firebaseConfig);
+  
---
+++ main.js
@@ -0,0 +1,9 @@
+function adduser(){
+var t = document.getElementById("dfi").value;
+if (t == "amover18"){
+window.location="chat.html"; 
+}
+else{
+window.alert("Please type 'amover18' in the text-box");
+}
+}
